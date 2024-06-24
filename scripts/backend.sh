74e1293bb9e54fb03abf43bd24faf2c8b4b33822
dhkehd2 <dhkehd2@gmail.com>
2024-06-24 12:56:33 +0900

    🎨 구조 잡기
    디렉토리 구조 & env & docker file

---
+++ scripts/backend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# chmod +x enter-backend.sh
+
+# Docker Compose 서비스 이름
+SERVICE_NAME="backend"
+
+# 실행 중인 컨테이너 ID 가져오기
+CONTAINER_ID=$(docker-compose ps -q $SERVICE_NAME)
+
+# 컨테이너가 실행 중인지 확인
+if [ -z "$CONTAINER_ID" ]; then
+  echo "Error: $SERVICE_NAME 컨테이너가 실행 중이 아닙니다."
+  exit 1
+fi
+
+# 컨테이너에 접속
+docker exec -it $CONTAINER_ID /bin/bash
